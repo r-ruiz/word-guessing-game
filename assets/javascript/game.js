d8d8ff091363aa55257f674a93e4e3da1a3bf829
ruben-r <ruben.ruiz@outlook.com>
2018-04-19 21:26:28 -0500

    fixed logic and guessLeft counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,21 +12,99 @@
 // array of word choices
 var computerChoices = ["picard", "data", "1701d", "holodeck", "borg", "klingon", "traveler", "starfleet", "starbase", "stardate", "jefferiestube", "warp", "vulcan", "romulan", "tasha", "geordi", "crusher", "troi", "wessley", "barkley", "riker", "galaxyclass", "constitutionclass", "transporters", "phasers", "photon", "worf", "bridge", "engineering", "tenforward", "isolinear", "computer", "cargobay", "riza", "commander", "captain", "numberone", "readyroom", "turbolift", "shuttlecraft", "quarters", "replicator", "primedirective", "deltaquadrant", "alphaquadrant", "admiral", "command", "cloak", "warbird", "roddenberry", "franks", "wheaton", "mcfadden", "stewart", "spiner", "burton", "laforge", "positronic", "security", "sheilds", "dorn", "gamaquadrant"];
 
-// Creating variables to hold the number of tries, user guesses, and building the blank spaces for the random word.
+// Creating variables to hold the number of tries, global random word, user guesses, and building the blank spaces for the random word.
 
-var maxTries = 10;
+var guessLeft = 10;
+var computerWord;
 var userHistory = [];
 var guessArray = [];
 
 function preGame(){
   // Randomly chooses a choice from the options array. This is the Computer's guess.
-  var computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
   //Build solution array
   for (var i = 0; i < computerWord.length; i++){
     guessArray[i] = "_";
   }
-    document.getElementById("gamegrid").innerHTML= guessArray.join(" ");
-    document.getElementById("message").innerHTML= "Enter a letter then press guess, or press quit to stop playing."
+    document.getElementById("gamegrid").innerHTML = guessArray.join(" ");
+    document.getElementById("status").innerHTML = "Enter a letter then press guess, or press quit to give up, or press reboot for new word.";
+    document.getElementById("counter").innerHTML= 10;
 }
-preGame();
 
+preGame(); // initialize the game
+
+// Ends the game when there are no more tries
+function fail() {
+    document.getElementById("status").innerHTML = "Access Denied!"
+    document.getElementById("counter").innerHTML = guessLeft;
+}
+
+function logic(){
+  var guess = document.getElementById("guess").value;
+  var updatedStatus = ""; // track the changes to status message
+
+  if (guessLeft === 0){
+    return fail();
+  }
+
+  if (guess.length !== 1){
+    updatedStatus = "You can only enter one letter";
+  }
+  else {
+    for (i = 0; i < computerWord.length; i++){
+      if (computerWord[i] === guess){
+        guessArray[i] = guess;
+        updatedStatus = guess + " is in the access code word";
+      }
+    }
+
+  // set a variable with a value of unguessed letters
+  var whatsLeft = guessArray.length;
+
+  // count the number of remaining missing letters
+  for (i = 0; i < guessArray.length; i++) {
+    if (guessArray[i] !== '_') {
+        whatsLeft -= 1;
+    }
+  }
+
+  // check to see if the puzzle has been solved
+    if (whatsLeft == 0) {
+        updatedStatus = "Access Granted";
+    }
+
+    // (otherwise) if we have no message, wrong guess 
+    if (updatedStatus === "") {
+      updatedStatus = "Sorry, no " + guess;
+      guessLeft -= 1;
+    }
+
+    userHistory.push(guess);
+
+    // Update the puzzle
+    document.getElementById("gamegrid").innerHTML = guessArray.join(" ");
+
+    // Clear out the previous entry
+    document.getElementById("guess").value = "";
+  }
+  if (guessLeft === 0){
+    return fail();
+  }
+  else {
+    document.getElementById("counter").innerHTML = guessLeft;
+    document.getElementById("status").innerHTML = updatedStatus;
+    document.getElementById("letterhistory").innerHTML = userHistory;
+  }
+  
+}
+
+// End the game and show the answer
+function quit() {
+  document.getElementById("status").innerHTML = "The access code word was " + computerWord;
+  document.getElementById("gamegrid").innerHTML = computerWord;
+}
+
+// Refresh the page when user click on reboot button
+function reboot() {
+  location.reload();
+}
